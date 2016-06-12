61318092633caded15c234a1cc73b875bcf22a79
Curtis Mayott <tinyiota@localhost.localdomain>
2016-06-11 23:30:17 -0600

    added more sqlite connection information, the port is 8080. added functions for accessing users by id and users by group (it will list all of the users in the group

--- Server/app/app.py
+++ Server/app/app.py
@@ -3,17 +3,49 @@
 import bottle
 import bottle.ext.sqlite
 from bottle import SimpleTemplate
+import sqlite3
 
 app = bottle.Bottle()
-plugin = bottle.ext.sqlite.Plugin(dbfile='/home/tinyiota/development/RoomLike/Server/Server/db/test.db')
-app.install(plugin)
 
-@app.route('/show/:name')
-def show(name, db):
-	
-        row = db.execute('SELECT * FROM Users WHERE UserName = ?', [name]).fetchone()
-        if row:
-		return row['UserName']
-        return HTTPError(404, 'Page not found')
+@app.route('/user/:userID')
+def user(userID):
+	conn = sqlite3.connect('/home/tinyiota/development/RoomLike/Server/Server/db/test.db')
+	cursor = conn.cursor()
+        cursor.execute('SELECT * FROM Users WHERE UserID = ?', [userID])
+	result = ""
+	while True:
+		row = cursor.fetchone()
+		if row == None:
+			break
+		result = result + ' ' + str(row[0]) + ' ' + str(row[1])
+	return result
+
+@app.route('/groups_users/:groupID')
+def groups_users(groupID):
+	conn = sqlite3.connect('../db/test.db')
+	cursor = conn.cursor()
+	cursor.execute('SELECT UsersGroups.UserID, Users.UserName FROM UsersGroups INNER JOIN Users ON UsersGroups.UserID = Users.UserID WHERE UsersGroups.GroupID = ?', [groupID])
+	result = ""
+	while True:
+		row = cursor.fetchone()
+		if row == None:
+			break
+		result = result + ' ' + str(row[0]) + ' ' + str(row[1])
+	return result
+
+@app.route('/get_chores/:userID')
+def get_chores(userID)
+	conn = sqlite3.connect('../db/test.db')
+	cursor = conn.cursor()
+	#edit this part...
+	cursor.execute('SELECT UsersGroups.UserID, Users.UserName FROM UsersGroups INNER JOIN Users ON UsersGroups.UserID = Users.UserID WHERE UsersGroups.GroupID = ?', [groupID])
+	result = ""
+	while True:
+		row = cursor.fetchone()
+		if row == None:
+			break
+	result = result + ' ' + str(row[0]) + ' ' + str(row[1])
+	return result
+
 
 app.run(host = 'localhost', port = 8080, debug = True)
